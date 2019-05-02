ad62227683a4db84a1a9f8cf332103d96dbfbc06
Sameera Leola <pixelandbyte@gmail.com>
2019-05-02 16:43:40 -0500

    Completed UI setup

---
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Sameera Roussi on 5/2/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectDarkThemeButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func selectPurpleThemeButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
