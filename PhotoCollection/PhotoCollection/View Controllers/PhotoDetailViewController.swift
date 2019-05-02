9d4248257313e932396f4d25adf1c3a08606ba63
Sameera Leola <pixelandbyte@gmail.com>
2019-05-02 16:24:58 -0500

    Added controls to the view and added the detail view and necessary  view controls

---
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollection
+//
+//  Created by Sameera Roussi on 5/2/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBAction func addPhotoButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Sameera Roussi on 5/2/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+    
+    
+    
+    
+}
